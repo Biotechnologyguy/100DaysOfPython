009dbfbcd11289ced5ef6260d3586d5c30437263
biotechnologyguy <shubhamvt121@gmail.com>
2023-10-28 18:40:32 +0530

    Band Name generator program, String manipulation, variables etc.

---
+++ day1/main.py
@@ -0,0 +1,16 @@
+print("Hello World!\nHello World!\nHello World!")
+print("Hello "+"Shubham")
+
+# Indentation is crucial in python
+# Below code will not work
+#     print("Hello "+"Shubham")
+
+# input function in python
+name = input("What is your name? ")
+# We can use f"{}" similar to template string in javascript
+print(f"Welcome {name}!!")
+
+# Variable naming
+# 1) No Spaces allowed
+# 2) Use underscore for spaces
+# 3) Can not start with number
