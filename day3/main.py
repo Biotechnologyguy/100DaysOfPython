f83cfcfcb20c47bd42137c63f68afbc374bb6a80
biotechnologyguy <shubhamvt121@gmail.com>
2023-12-06 17:52:35 +0530

    More conditions to check

--- day3/main.py
+++ day3/main.py
@@ -1,15 +1,22 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm?"))
-
+bill = 0
 if height > 120:
     print("You can ride the rollercoaster")
     age = int(input("What's your age?"))
     if age < 12:
-        print("Please pay $5")
+        print("Child tickets are $5")
+        bill += 5
     elif age < 18:
-        print("Please pay $7")
+        print("Youth tickets are $7")
+        bill += 7
     else:
-        print("Please pay $12")
+        print("Adult tickets are $12")
+        bill += 12
+    wants_photo = input("Do you want a photo taken? Y or N")
+    if wants_photo == "Y":
+        bill += 3
+    print(f"Total bill is {bill}$")
 else:
     print("You can't ride the rollercoaster")
 
