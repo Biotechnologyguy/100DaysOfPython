eb6bd762a47744024887c95ca369fc599e285f7a
biotechnologyguy <shubhamvt121@gmail.com>
2023-12-06 12:57:14 +0530

    Basics of conditionals in python

---
+++ day3/main.py
@@ -0,0 +1,15 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What's your age?"))
+    if age < 12:
+        print("Please pay $5")
+    elif age < 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
+else:
+    print("You can't ride the rollercoaster")
+
