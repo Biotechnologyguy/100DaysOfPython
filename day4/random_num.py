6fa411896d912fbd06061fabb8140933f9f41e50
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-20 22:52:32 +0530

    random module in python

---
+++ day4/random_num.py
@@ -0,0 +1,12 @@
+import random
+
+# both a and b are inclusive
+print(random.randint(0, 1000))
+
+# Below method creates the random float number between 0 and 1(exclusive)
+print(random.random())
+
+# how can we get random float between 0 and 10?
+# multiply above random number by 10
+print(random.random() * 10)
+
