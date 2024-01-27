56c30db4c765561a9c16a2b10577d8cdc9f3a563
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-27 21:57:21 +0530

    Day 8 : positional vs keyword argument

--- day8/main.py
+++ day8/main.py
@@ -17,6 +17,18 @@ def greet_with_name(name):
 
 # Here name is called argument
 greet_with_name("Angela")
-greet_with_name("Billie")
+# greet_with_name("Billie")
 
+
+# Functions with more than one input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What it is like in {location}")
+
+
+# Positional argument
+greet_with("Adam", "Germany")
+
+# keyword argument
+greet_with(location="London", name="Angela")
 
