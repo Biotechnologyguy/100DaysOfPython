300c9ff6c08ddbd958466d7fba9f15d2bbf3562c
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-27 14:23:58 +0530

    Day 7 challenge 3 solution code

--- day7/hangman.py
+++ day7/hangman.py
@@ -13,7 +13,7 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-# TODO-1: - Use a while loop to let the user guess again.
+# Use a while loop to let the user guess again.
 #  The loop should only stop once the user has guessed all the letters in the chosen_word
 #  and 'display' has no more blanks ("_").
 #  Then you can tell the user they've won.
