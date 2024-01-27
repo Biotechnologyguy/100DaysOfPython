d635bacfc0496261b209fb1d6e0328c14d74aaf5
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-27 14:10:43 +0530

    Day 7 challenge 3 starting code

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,31 +1,30 @@
+# Step 3
+
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
-
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-# Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
-# with 5 "_" representing each letter to guess.
+# Create blanks
 display = []
-for letter in range(len(chosen_word)):
-    display.append("_")
+for _ in range(word_length):
+    display += "_"
+
+# TODO-1: - Use a while loop to let the user guess again.
+#  The loop should only stop once the user has guessed all the letters in the chosen_word
+#  and 'display' has no more blanks ("_").
+#  Then you can tell the user they've won.
 
 guess = input("Guess a letter: ").lower()
 
-# Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for index in range(len(chosen_word)):
-    if chosen_word[index] == guess:
-        display[index] = guess
+# Check guessed letter
+for position in range(word_length):
+    letter = chosen_word[position]
+    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
 
-# Print 'display' and you should see the guessed letter
-#  in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 print(display)
