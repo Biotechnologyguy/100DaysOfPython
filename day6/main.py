df872bd96cae9b06ea5a360faf9e0e8a0028722f
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-27 09:31:32 +0530

    Day 6 EOD progress

---
+++ day6/main.py
@@ -0,0 +1,11 @@
+# Example of built-in functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+def print_hello():
+    print("Hello World")
+    print("This is basic python function")
+
+print_hello()
---
+++ day6/while_loop.py
@@ -0,0 +1,12 @@
+# Printing numbers between 0 and 10
+num = 0
+while num <= 10:
+    print(num)
+    num += 1
+
+# for loop is great to iterate and do something with each of the items
+
+# while loop is when we don't care about the index and items
+# While loop is more risky in case of infinite loop
+
+
