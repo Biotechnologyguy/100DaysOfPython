6c86cacc88fb2f9816e352c93eef3b17c478b5e2
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-28 14:34:47 +0530

    Day 8 : caesar cipher decoding logic

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -1,21 +1,13 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
-    # Inside the 'encrypt' function, shift each letter of the 'text'
-    #  forwards in the alphabet by the shift amount and print the encrypted text.
-    # e.g.
-    # plain_text = "hello"
-    # shift = 5
-    # cipher_text = "mjqqt"
-    # print output: "The encoded text is mjqqt"
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
@@ -27,11 +19,32 @@ def encrypt(plain_text, shift_amount):
     print(f"The encoded text is {cipher_text}")
 
 
-# HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+# Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(encrypted_text, shift_number):
+    decrypted_text = ""
+    # Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by
+    #  the shift amount and print the decrypted text.
+    # e.g.
+    # cipher_text = "mjqqt"
+    # shift = 5
+    # plain_text = "hello"
+    # print output: "The decoded text is hello"
+    for letter in encrypted_text:
+        old_position = alphabet.index(letter)
+        new_position = old_position - shift_number
+        while new_position < 0:
+            new_position += len(alphabet)
+        new_letter = alphabet[new_position]
+        decrypted_text += new_letter
+    print(f"Decrypted text is {decrypted_text}")
 
-# Call the encrypt function and pass in the user inputs.
-#  You should be able to test the code and encrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+
+# Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable.
+#  Then call the correct function based on that 'drection' variable.
+#  You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(encrypted_text=text, shift_number=shift)
 
 
