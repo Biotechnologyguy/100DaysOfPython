ef967be8bb18dbb4e7f6714e29d95afaa659b1cc
biotechnologyguy <shubhamvt121@gmail.com>
2024-01-28 16:41:32 +0530

    Day 8 : caesar cipher code optimization, better user experience and now can also include spaces in text

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -1,28 +1,34 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from art import logo
 
 
 # Creating a single function caesar() for both encode and decode
 def caesar(direction_of_caesar, plain_text, shift_number):
     cipher_text = ""
     for letter in plain_text:
-        position = alphabet.index(letter)
-        if direction_of_caesar == "encode":
-            new_position = position + shift_number
-            while new_position > len(alphabet):
-                new_position -= len(alphabet)
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if direction_of_caesar == "encode":
+                new_position = position + shift_number
+                while new_position > len(alphabet):
+                    new_position -= len(alphabet)
+            else:
+                new_position = position - shift_number
+                while new_position < 0:
+                    new_position += len(alphabet)
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
         else:
-            new_position = position - shift_number
-            while new_position < 0:
-                new_position += len(alphabet)
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+            cipher_text += " "
     print(f"The {direction_of_caesar}d text is {cipher_text}")
 
 
-caesar(direction_of_caesar=direction, plain_text=text, shift_number=shift)
-
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+repeat = True
+while repeat:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction_of_caesar=direction, plain_text=text, shift_number=shift)
+    repeat = input("Would you like to continue the programa? yes or no\n").lower() == "yes"
