50292a986c4abf07960146efce649b5260b6dabb
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-04 00:03:48 +0530

    Day 10 : calculator with n calculations

--- day10/calculator.py
+++ day10/calculator.py
@@ -30,18 +30,20 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number? : "))
 
+num1 = int(input("What's the first number? : "))
 for key in operations:
     print(key)
-
 operation_symbol = input("Pick an operation from above line : ")
 num2 = int(input("What's the Second number? : "))
-
 first_answer = operations[operation_symbol](num1, num2)
 print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-operation_symbol = input("Pick another operation : ")
-num3 = int(input("What's the next number? : "))
-second_answer = operations[operation_symbol](first_answer, num3)
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+continue_operation = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit : ") == "y"
+while continue_operation:
+    operation_symbol = input("Pick another operation : ")
+    num3 = int(input("What's the next number? : "))
+    second_answer = operations[operation_symbol](first_answer, num3)
+    print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+    first_answer = second_answer
+    continue_operation = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to exit : ") == "y"
 
