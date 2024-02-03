9c0a99731a98d46e4e84821bcbd97812429086b9
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-04 00:19:09 +0530

    Day 10 : calculator with n calculations, code optimization

--- day10/calculator.py
+++ day10/calculator.py
@@ -31,19 +31,22 @@ operations = {
 }
 
 
-num1 = int(input("What's the first number? : "))
-for key in operations:
-    print(key)
-operation_symbol = input("Pick an operation from above line : ")
-num2 = int(input("What's the Second number? : "))
-first_answer = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-continue_operation = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit : ") == "y"
-while continue_operation:
-    operation_symbol = input("Pick another operation : ")
-    num3 = int(input("What's the next number? : "))
-    second_answer = operations[operation_symbol](first_answer, num3)
-    print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
-    first_answer = second_answer
-    continue_operation = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to exit : ") == "y"
+def calculator():
+    continue_operation = True
+    num1 = int(input("What's the first number? : "))
+    for key in operations:
+        print(key)
+    while continue_operation:
+        operation_symbol = input("Pick an operation : ")
+        num2 = int(input("What's the next number? : "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a "
+                                   f"new calculation : ") == "y":
+            num1 = answer
+        else:
+            continue_operation = False
+            calculator()
 
+
+calculator()
