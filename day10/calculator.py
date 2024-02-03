726d9270fea2ca158b8a12bc71db1b36750e1f62
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-03 16:24:35 +0530

    Day 10 : calculator working code for single operation without loop

--- day10/calculator.py
+++ day10/calculator.py
@@ -31,12 +31,12 @@ operations = {
 }
 
 num1 = int(input("What's the first number? : "))
-num2 = int(input("What's the Second number? : "))
 
 for key in operations:
     print(key)
 
 operation_symbol = input("Pick an operation from above line : ")
+num2 = int(input("What's the Second number? : "))
 
 answer = operations[operation_symbol](num1, num2)
 print(f"{num1} {operation_symbol} {num2} = {answer}")
