e9231f80f0cc02b81680cdba9fcf55b604ea43ab
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-10 14:13:58 +0530

    Day 17 : Adding methods to class

--- day17/main.py
+++ day17/main.py
@@ -1,15 +1,23 @@
 class User:
 
     def __init__(self, user_id, username):
-        print("New user is beeing created")
+        # print("New user is beeing created")
         self.id = user_id
         self.username = username
         self.followers = 0
-    pass
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User(1, "Shubham")
 user_2 = User(2, "Angela")
+user_2.follow(user_1)
 
-print(user_1)
-print(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
