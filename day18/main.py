74d72c5790d72e90c192c2c7b1842f6f29ff5103
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-11 14:30:24 +0530

    day 18 : Challenge-2 :   Draw the dotted line - solution

--- day18/main.py
+++ day18/main.py
@@ -1,14 +1,34 @@
 from turtle import Turtle, Screen
 
+# importing all classes from module(UNUSUAL IN PYTHON COMMUNITY)
+# from turtle import *
+
+# Alisaing modules
+import heroes as h
+print(h.gen())
+
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("orange")
+
+
 # Challenge-1 Draw the square
-for _ in range(0, 4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+def draw_square():
+    for _ in range(0, 4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(90)
 
 
+# Draw dotted line
+for _ in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+
+
+
+# draw_square()
 
 
 
