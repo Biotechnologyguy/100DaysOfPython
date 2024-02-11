60cb4454b5b3b259cfc3f08c080357f34bacd475
biotechnologyguy <shubhamvt121@gmail.com>
2024-02-11 13:04:16 +0530

    day 18 : Challenge 1 Draw the square - solution

---
+++ day18/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+# Challenge-1 Draw the square
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("orange")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
