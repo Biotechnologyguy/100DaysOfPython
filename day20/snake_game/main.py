c049a32eee3e34bf027626a683eee63ba4ef620a
Shubham Thorat <shubhamvt121@gmail.com>
2024-03-08 16:28:33 +0530

    day 20 : created snake body

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -5,12 +5,13 @@ screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+# each turtle is of 20 * 20 px
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-
-
-
-
-
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
