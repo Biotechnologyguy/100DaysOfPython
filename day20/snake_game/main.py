9a70e62bb9ba1859f24af53743c0d56593bd21eb
Shubham Thorat <shubhamvt121@gmail.com>
2024-03-08 15:44:45 +0530

    day 20 : Snake game screen setup

---
+++ day20/snake_game/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
