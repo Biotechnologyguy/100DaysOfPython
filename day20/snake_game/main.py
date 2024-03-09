e4c7d908bbc8cfa265acf824df2dfa5816e3af76
Shubham Thorat <shubhamvt121@gmail.com>
2024-03-09 22:14:15 +0530

    day 20 : snake movement

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -7,13 +7,18 @@ screen.title("My Snake Game")
 
 # each turtle is of 20 * 20 px
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
+segments = []
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
-
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
